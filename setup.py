98385a9563659783bd476579c064ba96db6f3f05
c7021101 <obmann.daniel@live.de>
2020-09-24 10:52:54 +0200

    ADD: setup files

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name="utility",
+      version="0.0.1",
+      description="Utility functions for training networks.",
+      packages=["utility"],
+      install_requires=['tensorflow', 'numpy'])
